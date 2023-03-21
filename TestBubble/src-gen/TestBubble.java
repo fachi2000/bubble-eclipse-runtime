f5fda016940dce09958552218d8271f504da114a
lukasjankuss <lukasjankuss@gmail.com>
2023-03-21 15:57:21 +0000

    jar + junit test

---
+++ TestBubble/src-gen/TestBubble.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+public class TestBubble {
+    public static void main(String[] args) {
+        printRecipes();
+    }
+
+    public static void printRecipes() {
+        System.out.println("Recipe: matcha_bubble_tea");
+        System.out.println("  Ingredients:");
+        System.out.println("    - matcha: 2.5");
+        System.out.println("    - milk: 1.0");
+        System.out.println("    - sweetener: 1.5");
+        System.out.println("  Toppings:");
+        System.out.println("    - boba: 0.5");
+        System.out.println("  Brewing:");
+        System.out.println("    - Tea: green_tea");
+        System.out.println("    - Ice: 100");
+        System.out.println("    - Sugar: 50");
+        System.out.println("  Serving:");
+        System.out.println("    - Size: large");
+        System.out.println("  Pearls:");
+        System.out.println("    - Included");
+        System.out.println();
+    }
+}
